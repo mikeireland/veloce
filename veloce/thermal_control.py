f951f1c5580a997273ae1d47afe9f7ed1e998a11
Mike Ireland <michael.ireland@anu.edu.au>
2017-03-02 06:22:37 +1100

    Added heater test function.

--- veloce/thermal_control.py
+++ veloce/thermal_control.py
@@ -10,6 +10,7 @@ Version history
 from labjack import ljm
 LABJACK_IP = "192.168.1.3"
 HEATER_DIOS = ["0"]         #Input/output indices of the heaters
+PWM_MAX =1000000
 
 class ThermalControl:
     def __init__(self, ip=None):
@@ -42,8 +43,8 @@ class ThermalControl:
         #(and FIO4 upwards is only available via one of the DB connectors)
         self.current_heaters = np.zeros(len(HEATER_DIOS))
         aNames = ["DIO_EF_CLOCK0_DIVISOR", "DIO_EF_CLOCK0_ROLL_VALUE", "DIO_EF_CLOCK0_ENABLE"]
-        #Set the first number below to 256 for testing.
-        aValues = [8, 1000000, 1]
+        #Set the first number below to 256 for testing on a multimeter.
+        aValues = [8, PWM_MAX, 1]
         for dio in HEATER_DIOS:
             aNames.extend(["DIO"+dio+"_EF_INDEX", "DIO"+dio+"_EF_CONFIG_A", "DIO"+dio+"_EF_ENABLE"])
             aValues.extend([0,0,1])
@@ -57,10 +58,43 @@ class ThermalControl:
         ljm.close(self.handle)
         self.labjack_open=True
         
+    def cmd_heater(self, the_command):
+        """Set a single heater to a single PWM output"""
+        the_command = the_command.split()
+        #Error check the input (for bugshooting, do this
+        #even if the labjack isn't open)
+        if len(the_command) != 3:
+            return "Useage: HEATER [heater_index] [fraction]"
+        else:
+            try:
+                dio_index = int(the_command[1])
+            except:
+                return "ERROR: heater_index must be an integer"
+            try:
+                dio = HEATER_DIOS[dio_index]
+            except:
+                return "ERROR: heater_index out of range"
+            try:
+                fraction = float(the_command[2])
+            except:
+                return "ERROR: fraction must be between 0.0 and 1.0"
+            if (fraction < 0) or (fraction > 1):
+                return "ERROR: fraction must be between 0.0 and 1.0"
+        #Check that the labjack is open
+        if not self.labjack_open:
+            raise UserWarning("Labjack not open!")
+                
+        #Now we've error-checked, we can turn the heater fraction to an
+        #integer and write tot he labjack
+        aNames = ["DIO"+dio+"_EF_CONFIG_A"]
+        aValues = [int(fraction * PWM_MAX)]
+        numFrames = len(aNames)
+        results = ljm.eWriteNames(self.handle, numFrames, aNames, aValues)
+        
     def cmd_setgain(self, the_command):
         """Dummy gain setting function"""
-        the_command.split()
-        if len(the_command)==1:
+        the_command = the_command.split()
+        if len(the_command)!=2:
             return "Useage: SETGAIN [newgain]"
         else:
             return "Gain not set to {}".format(the_command[1:])
