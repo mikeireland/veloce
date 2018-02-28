e3fd96a086c47589c45984532d8cfa46e03252ee
Mike Ireland <michael.ireland@anu.edu.au>
2018-02-28 15:52:30 +1100

    Added comments.

--- utils/fwhm_computation.py
+++ utils/fwhm_computation.py
@@ -7,27 +7,31 @@ import scipy.ndimage as nd
 from astropy.modeling import models, fitting
 plt.ion()
 
-fnames = glob.glob('QArgon?-50um-?--???.fits')
-focus = [int(f[16:19]) for f in fnames]
-nfirst=5
-threshold=500
-
-fnames = glob.glob('../Feb27/Qe*fits')
-focus = np.zeros(len(fnames))
+fnames = glob.glob('/Users/mireland/data/veloce/Feb28/QE*fits')
+focus = focus = [int(f[-8:-5]) for f in fnames]
 nfirst=3
-threshold=1000
+threshold=2000
+display_it=False
 
 #------------------------------------------
 
 szx = pyfits.getheader(fnames[0])['NAXIS1']
 szy = pyfits.getheader(fnames[0])['NAXIS2']
+
+#First lets read in the first few frames, and take a median to search for 
+#bright lines. We do this to avoid cosmic rays.
 first_data = np.zeros((nfirst,szy,szx))
 for ix, fn in enumerate(fnames[:nfirst]):
         first_data[ix] = pyfits.getdata(fn)
-
 med_data = np.median(first_data, axis=0)
+
+#Now lets look for possible peaks amongst the difference between this median
+#frame and a smoothed version of it.
 smoothim = nd.median_filter(med_data,(3,3))
 wpeaks = np.where(med_data-smoothim > threshold)
+
+#Lets manually make sure we only pick each peak once by just using the first coordinate
+#we find within 5 pixels.
 peaks = []
 nfound = 0
 for xix, yix in zip(wpeaks[0], wpeaks[1]):
@@ -42,22 +46,20 @@ for xix, yix in zip(wpeaks[0], wpeaks[1]):
         if dpeak < 12:
             duplicate=True
             continue
-        
     if not duplicate:
         peaks.append([int(xix),int(yix)])
         nfound += 1
         if (nfound % 100 == 0):
             print("Done {:d} peaks".format(nfound))
-
 peaks = np.array(peaks)
-import pdb; pdb.set_trace()
 
-#Now lets run through the images
+#Now lets run through the images and fit a 2D Gaussian in the vicinity of each peak.
 peakparams = np.zeros( (len(fnames),peaks.shape[0], 6) )
 hw = 5
 fit_p = fitting.LevMarLSQFitter()
 xy_fit = np.meshgrid(np.arange(2*hw), np.arange(2*hw))
 for j, fn in enumerate(fnames):
+    print("Doing file {:d} of {:d}".format(j, len(fnames)))
     dd = pyfits.getdata(fn)
     for i, xyix in enumerate(peaks):
         subarr = dd[xyix[0]-hw:xyix[0]+hw, xyix[1]-hw:xyix[1]+hw]
@@ -72,7 +74,7 @@ for j, fn in enumerate(fnames):
             x_stddev = 3.0, y_stddev = 3.0, fixed={"theta":True})
         p = fit_p(p_init, xy_fit[0], xy_fit[1], subarr)
         peakparams[j,i] = [p.amplitude.value, p.x_mean.value, p.y_mean.value, p.x_stddev.value, p.y_stddev.value, p.theta.value]
-        if (peakparams[j,i,4]<1.5/2.35) and (i != 2):
+        if ((peakparams[j,i,4]<1.5/2.35) and (i != 2) and display_it==True):
             print(focus[j])
             print(xyix)
             print(p)
